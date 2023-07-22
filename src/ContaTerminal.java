0892f719e014617230fa6659e1584029edae58d8
Andreliveira <andreliveira0@gmail.com>
2023-07-22 15:43:56 -0300

    desfio básico java banco conta completo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
-    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao atemdimemto online de criação de contas!\n");
+
+        System.out.println("Por gentileza insira seu nome completo: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por gentileza insira a agência desejada: ");
+        String agencia = scanner.next();
+        System.out.println("Por gentileza insira o número da conta: ");
+        Integer numero = scanner.nextInt();
+        System.out.println("Por gentileza insira seu saldo: ");
+        Double saldo = scanner.nextDouble();
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque!");
+
+        scanner.close();
+ }
 }
